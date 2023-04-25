3bd447b4d5bd65e321a8b89ebffde97589882c75
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-24 22:42:29 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -2,7 +2,7 @@ gsap.utils.toArray(".parallax").forEach((section, i) => {
   section.bg = section.querySelector(".bg"); 
 
   // Give the backgrounds some random images
-  section.bg.style.backgroundImage = `url(https://github.com/dstlmike/chat1/blob/master/images/Screenshot_20221204-214353_Chrome.jpg)`;
+  section.bg.style.backgroundImage = `url(images/Screenshot_20221204-214353_Chrome.jpg)`;
 
   // Do the parallax effect on each section
   if (i) {

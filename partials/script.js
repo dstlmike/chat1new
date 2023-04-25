a006895d612cd6fd9b3f691884b249b2e0ae5968
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-25 02:28:41 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -3,16 +3,21 @@ gsap.utils.toArray(".parallax").forEach((section, i) => {
 //section.bgb = section.querySelector(".bgb"),
 //section.bgc = section.querySelector(".bgc"),
 //section.bgd = section.querySelector(".bgd");
+var myPix = new Array("Screenshot_20221204-214353_Chrome.jpg", "Screenshot_20221204-214401_Chrome.jpg", "Screenshot_20221204-214455_Chrome.jpg");
+   var randomNum = Math.floor(Math.random() * myPix.length);
 
   // Give the backgrounds some random images
- // section.bg.style.backgroundImage = `url(Screenshot_20221204-214353_Chrome.jpg)`;
+ section.bg.style.backgroundImage = `url(randomNum)`;
 //  section.bgb.style.backgroundImage = `url(Screenshot_20221204-214401_Chrome.jpg)`;
   //section.bgc.style.backgroundImage = `url(Screenshot_20221204-214455_Chrome.jpg)`;
 //section.bgd.style.backgroundImage = `url(Screenshot_20221204-214455_Chrome.jpg)`;
 
   // Do the parallax effect on each section
   if (i) {
-    section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
+//var myPix = new Array("Screenshot_20221204-214353_Chrome.jpg", "Screenshot_20221204-214401_Chrome.jpg", "Screenshot_20221204-214455_Chrome.jpg");
+ //  var randomNum = Math.floor(Math.random() * myPix.length);
+
+ section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
  // section.bgb.style.backgroundPosition = `50% ${innerHeight / 2}px`;
  // section.bgc.style.backgroundPosition = `50% ${innerHeight / 2}px`;
 //section.bgd.style.backgroundPosition = `50% ${innerHeight / 2}px`;

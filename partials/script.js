d975657b10575701905c554434da5e5932a08ec9
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-25 01:12:38 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -1,5 +1,5 @@
 gsap.utils.toArray(".parallax").forEach((section, i) => {
-  section.bg = section.querySelector(".bg"), section.querySelector(".bgb"), section.querySelector(".bgc"), section.querySelector(".bgd");
+  section.bg = section.querySelector(".bg") && section.querySelector(".bgb") && section.querySelector(".bgc") && section.querySelector(".bgd");
 //section.bgb = section.querySelector(".bgb"),
 //section.bgc = section.querySelector(".bgc"),
 //section.bgd = section.querySelector(".bgd");

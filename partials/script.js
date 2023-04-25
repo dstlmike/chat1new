2e5d8e853046fa9e4248df80dac00a7633e3d74c
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-24 22:44:03 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -5,7 +5,7 @@ gsap.utils.toArray(".parallax").forEach((section, i) => {
   section.bg.style.backgroundImage = `url(/images/Screenshot_20221204-214353_Chrome.jpg)`;
 
   // Do the parallax effect on each section
-  if (i) {
+//  if (i) {
     section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
 
     gsap.to(section.bg, {

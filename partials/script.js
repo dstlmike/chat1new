2c4b06c9ac61594301bb0c89d27e43f44d812a0f
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-25 01:22:03 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -6,16 +6,16 @@ gsap.utils.toArray(".parallax").forEach((section, i) => {
 
   // Give the backgrounds some random images
   section.bg.style.backgroundImage = `url(Screenshot_20221204-214353_Chrome.jpg)`;
-  section.bgb.style.backgroundImage = `url(Screenshot_20221204-214401_Chrome.jpg)`;
-  section.bgc.style.backgroundImage = `url(Screenshot_20221204-214455_Chrome.jpg)`;
-section.bgd.style.backgroundImage = `url(Screenshot_20221204-214455_Chrome.jpg)`;
+//  section.bgb.style.backgroundImage = `url(Screenshot_20221204-214401_Chrome.jpg)`;
+  //section.bgc.style.backgroundImage = `url(Screenshot_20221204-214455_Chrome.jpg)`;
+//section.bgd.style.backgroundImage = `url(Screenshot_20221204-214455_Chrome.jpg)`;
 
   // Do the parallax effect on each section
   if (i) {
     section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
-  section.bgb.style.backgroundPosition = `50% ${innerHeight / 2}px`;
-  section.bgc.style.backgroundPosition = `50% ${innerHeight / 2}px`;
-section.bgd.style.backgroundPosition = `50% ${innerHeight / 2}px`;
+ // section.bgb.style.backgroundPosition = `50% ${innerHeight / 2}px`;
+ // section.bgc.style.backgroundPosition = `50% ${innerHeight / 2}px`;
+//section.bgd.style.backgroundPosition = `50% ${innerHeight / 2}px`;
 
     gsap.to(section.bg, {
       backgroundPosition: `50% ${-innerHeight / 2}px`,

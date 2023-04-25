43d5304cf1621d3b6b67075b9af6ce1df1aa85d6
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-24 22:27:08 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -6,10 +6,10 @@ gsap.utils.toArray(".parallax").forEach((section, i) => {
 
   // Do the parallax effect on each section
   if (i) {
-    section.bg.style.backgroundPosition = `50% ${innerHeight = 25%}`;
+    section.bg.style.backgroundPosition = `50%`;
 
     gsap.to(section.bg, {
-      backgroundPosition: `50% ${-innerHeight = 25%}`,
+      backgroundPosition: `50%`,
       ease: "none",
       scrollTrigger: {
         trigger: section,

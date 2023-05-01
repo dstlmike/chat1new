650b21ddc8ade24922dee3def05caa2d42a6d04f
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-30 23:33:23 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -28,7 +28,7 @@ section.bg.style.backgroundPosition = `100% ${innerHeight / 1}px`;
 
 
      gsap.to(section.bg, {
-      backgroundPosition: `100%` ${innerHeight / 1 }px`,
+      backgroundPosition: `100% ${innerHeight / 1}px`,
       ease: "none",
       scrollTrigger: {
         trigger: section,

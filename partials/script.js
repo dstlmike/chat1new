d70a993cf79ad73096871b5d0e67d98e5c3c9d38
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-05-06 21:48:29 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -4,7 +4,8 @@ gsap.utils.toArray(".parallax").forEach((section, i) => {
 var pics = ["url(skyline1.jpg)", 
 "url(skyline2.jpg)",
 "url(skyline3.jpg)",
-"url(skyline4.jpg)"
+"url(skyline4.jpg)",
+"url(movie.mp4)"
 ];
 var pic = document.querySelector('section');
 
